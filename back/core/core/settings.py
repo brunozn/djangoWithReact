eb8b85ef9abeb7bb21f0d7825d2d9f610d3432ae
brunojnd <brunojndias@gmail.com>
2021-01-31 02:34:41 -0300

    CRUD react e django

--- back/core/core/settings.py
+++ back/core/core/settings.py
@@ -127,4 +127,5 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
        'https://localhost:3000',
+       'http://127.0.0.1:3000',
 )
--- back/core/customers/models.py
+++ back/core/customers/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
-from django.db import models
 
 class Customer(models.Model):
     first_name = models.CharField("First name", max_length=255)
     last_name = models.CharField("Last name", max_length=255)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
-    address =  models.TextField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     createdAt = models.DateTimeField("Created At", auto_now_add=True)
 
